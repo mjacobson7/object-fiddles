9f2e97b406cf4d67c4fb5f1f82a2644ca7cf83a8
Max Jacobson <mjacobson7@gmail.com>
2016-05-14 20:51:37 -0600

    testing new terminal

---
+++ More_Practice.js
@@ -0,0 +1,25 @@
+// Have the function simpleAdding(num) add up all the numbers from 1 to num.
+myArr = [];
+
+function simpleAdding(num) {
+  for(var i = 0; i < num; i++) {
+    myArr.push(num);
+  }
+  return num;
+}
+simpleAdding(4);
+
+// For the test cases, the parameter num will be any number from 1 to 1000.
+
+
+// Write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
+
+
+// Write a function called 'longer' that is given arr1 and arr2 as its only arguments and returns the array that is longer.
+
+
+// Write a function called loveTheThrees that takes in an array full of numbers. It should loop through the array and adds any numbers that are divisible by 3 to a new array called threes. Once you have added all the numbers that are divisible by 3 to the new array, then return the threes array.
+
+
+// Write a function called addTen that is given 'numbers' as it's only argument and returns a new
+// array after adding ten to each item in numbers.
